05b8c32b1791214010d45b532e02fc48306268d8
gitworkflows <118260833+gitworkflows@users.noreply.github.com>
2025-02-16 16:34:21 +0600

    Create preview.js
    Signed-off-by: gitworkflows <118260833+gitworkflows@users.noreply.github.com>

---
+++ .storybook/preview.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react"
+import { ThemeProvider } from "styled-components"
+import { Flex, DefaultTheme, DarkTheme, GlobalStyles } from "@khulnasoft/khulnasoft-ui"
+
+const preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+    layout: "centered",
+    backgrounds: { disable: true },
+  },
+
+  decorators: [
+    (Story, context) => {
+      const theme = context.globals.theme === "dark" ? DarkTheme : DefaultTheme
+
+      return (
+        <ThemeProvider theme={theme}>
+          <GlobalStyles />
+          <Flex width="100vw" height="100vh" background="mainBackground" justifyContent="center">
+            <Story />
+          </Flex>
+        </ThemeProvider>
+      )
+    },
+  ],
+}
+
+export const globalTypes = {
+  theme: {
+    description: "Global theme for components",
+    defaultValue: "light",
+    toolbar: {
+      title: "Theme",
+      icon: "circlehollow",
+      items: ["light", "dark"],
+      dynamicTitle: true,
+    },
+  },
+}
+
+export default preview
